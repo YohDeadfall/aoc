38f1fde176a691bf4efeed5b249fde2c82a8ba84
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-12-07 21:57:20 +0300

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,28 @@
+pub(crate) fn run_part_1() {
+    let result = std::fs::read_to_string("inputs/day1.txt")
+        .unwrap()
+        .lines()
+        .flat_map(|s| s.parse::<usize>())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|w| w[0] < w[1])
+        .count();
+    
+    println!("day 1 part 1: {}", result);
+}
+
+pub(crate) fn run_part_2() {
+    let result = std::fs::read_to_string("inputs/day1.txt")
+        .unwrap()
+        .lines()
+        .flat_map(|s| s.parse::<usize>())
+        .collect::<Vec<_>>()
+        .windows(3)
+        .map(|w| w.iter().sum::<usize>())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|w| w[0] < w[1])
+        .count();
+    
+    println!("day 1 part 2: {}", result);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    day1::run_part_1();
+    day1::run_part_2();
+}
